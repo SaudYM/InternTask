498cd5f86f693ce53cd9a298c9df5829a1c1611b
SaudYM <alyamisaud7@gmail.com>
2023-05-24 20:05:04 +0300

    sidebar and top bar done

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,15 +3,18 @@ import Studnet from "./Studnet";
 import Payment from "./Payment";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import TopBar from "./TopBar";
 export default function LandingPage() {
   return (
     <BrowserRouter>
       <Sidebar>
-        <Routes>
-          <Route path="/DashBoard" element={<DashBoard />} />
-          <Route path="/Studnet" element={<Studnet />} />
-          <Route path="/Payment" element={<Payment />} />
-        </Routes>
+        <TopBar>
+          <Routes>
+            <Route path="/DashBoard" element={<DashBoard />} />
+            <Route path="/Studnet" element={<Studnet />} />
+            <Route path="/Payment" element={<Payment />} />
+          </Routes>
+        </TopBar>
       </Sidebar>
     </BrowserRouter>
   );
